e2687993b667cbafcff8e79a29fdebda9a1ad9cb
jepril <hongranhxr@163.com>
2019-11-20 23:43:21 +0800

    1

---
+++ 36.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+import	"encoding/json" 
+
+type	IT	struct{
+	Company	string	`json:"company"`
+	Subjects	[]string
+	Isok	bool				`json:",string"`
+	Prince	float64
+}
+
+func main(){
+	s := IT{"itcast",[]string{"Go","c++","python"},true,666.66}
+	buf , err :=json.MarshalIndent(s,"   ","    ")
+	if err !=nil{
+		fmt.Println("err=",err)
+		return
+	}
+	fmt.Println("buf = ",string(buf))
+}
---
+++ 37.go
@@ -0,0 +1,55 @@
+package main
+
+import	(
+	"fmt"
+	"io"
+	"os"
+)
+
+func Writefile(path string)  {
+	f	,err	:=os.Create(path)
+	if err != nil {
+		fmt.Println("err	=	",err)
+		return 
+	}
+	defer	f.Close()
+
+	var	buf	string
+
+	for i := 0; i < 10; i++ {
+		buf	=	fmt.Sprintf("i=%d\n",i)
+		fmt.Println("buf = ",buf)
+		
+		n,err	:=	f.WriteString(buf)
+		if err != nil {
+			fmt.Println("err	=	",err)
+			return 
+		}
+		fmt.Println("n	=	",n)
+	}
+}
+
+func Readfile(path string)  {
+	f , err := os.Open(path)
+	if err != nil {
+	fmt.Println("err",err)
+	return
+	}
+
+	defer	f.Close()
+	buf := make([]byte, 1024*2)
+
+	n , err1 := f.Read(buf)
+	if err != nil && err != io.EOF {
+		fmt.Println("err1 = ",err1)
+		return 
+	}
+
+	fmt.Println("buf = ",string(buf[:n]))
+}
+func main(){
+	path := "./aaa.txt"
+
+	Writefile(path)
+	Readfile(path)
+}
